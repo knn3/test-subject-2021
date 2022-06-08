fcd44e419409f248cbb1803f5b522e6048c28860
Vinh <sptrodon@DESKTOP-2H76C6C.localdomain>
2022-06-08 16:31:10 -0700

    Adding reponse to appear on website

--- frontend/src/components/MainComponent.jsx
+++ frontend/src/components/MainComponent.jsx
@@ -1,19 +1,26 @@
+import { useState } from 'react';
+
+
 function LandingPage() {
+
+    const [msg, setMSG] = useState('');
 
     const CallAPI = async () => {
         console.log("You are running");
         const res = await fetch(
             "https://test-subject-2021.herokuapp.com/hello_world"
-        );
-        const msg = JSON.stringify(res);
-        console.log(msg);
-    }
+        ).then((response) => {
+            return response.json();
+        });
+        setMSG(res.greeting);
+    };
 
     return (
         <div>
             <button onClick={CallAPI}>Hello World</button>
+            <p>{msg}</p>
         </div>
-    )
+    );
 }
 
 export default LandingPage;
