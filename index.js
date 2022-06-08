1dffa710b5a04a54bcf84754c5929acceab8eb60
Vinh <sptrodon@DESKTOP-2H76C6C.localdomain>
2022-06-08 15:17:00 -0700

    Adding Cors policy

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const cors = require("cors");
 const app = require("express")();
+// app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
