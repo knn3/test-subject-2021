d3495332cafdc730f75f76dc5e2d811c75f94813
Vinh <sptrodon@DESKTOP-2H76C6C.localdomain>
2022-06-08 15:22:15 -0700

    Change res to json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cors = require("cors");
+const express = require("express");
 const app = require("express")();
 // app.use(cors());
+app.use(express.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
@@ -8,12 +10,12 @@ app.use((req, res, next) => {
         "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
-})
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.json({ greeting: "Hello World" });
 });
 
 app.get("/hello_world", (req, res) => {
