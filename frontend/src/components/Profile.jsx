ff60588ced6c4e078c09f1370d62bd863319aa7f
Nguyên Nguyễn <nguyen@d207-023-217-151.wireless.sfu.ca>
2022-06-12 12:54:41 -0700

    Auth0 complete

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import JSONPretty from 'react-json-pretty';
 
 const Profile = () => {
-    const { user } = useAuth0();
-  return (
-      <div>
-       {JSON.stringify(user, null, 2)}   
-    </div>
+    const { user, isAuthenticated } = useAuth0();
+    
+    return (
+        isAuthenticated && (
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+
+                <JSONPretty data={user} />
+            {/* {JSON.stringify(user, null, 2)}    */}
+            </div>
+        )
+      
   )
 }
 
