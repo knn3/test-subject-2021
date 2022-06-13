e80d1b10c38bf578fb9859ef890fedf5e06d27d0
Vinh <sptrodon@DESKTOP-2H76C6C.localdomain>
2022-06-13 11:17:04 -0700

    Testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,26 @@
-import './App.css'
-import LandingPage from './components/MainComponent'
-import { Routes, Route, Link } from 'react-router-dom';
-import Login from './components/Login';
+import "./App.css";
+import LandingPage from "./components/MainComponent";
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
+    return (
+        <div className="App">
+            <Link to="/test-subject-2021/login"> Login</Link>
+            <Link to="/test-subject-2021"> Home</Link>
 
-  return (
-      <div className="App">
-          <Link to="/login"> Login</Link>
-          <Link to="/"> Home</Link>
-
-          <Routes>
-              <Route path="/" element={<LandingPage />}></Route>
-              <Route path="/login" element={<Login />}></Route>
-          </Routes>
-      </div>
-  );
+            <Routes>
+                <Route
+                    path="/test-subject-2021"
+                    element={<LandingPage />}
+                ></Route>
+                <Route
+                    path="/test-subject-2021/login"
+                    element={<Login />}
+                ></Route>
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: "../"
-  },
-  plugins: [react()]
-})
+    base: "./test-subject-2021",
+    build: {
+        outDir: "../",
+    },
+    plugins: [react()],
+});
