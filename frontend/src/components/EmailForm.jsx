ca3de377c2026f22d37607140c17ed86697149fc
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-08-05 14:51:25 -0700

    commit rent_button branch

--- frontend/src/components/EmailForm.jsx
+++ frontend/src/components/EmailForm.jsx
@@ -20,7 +20,7 @@ export const EmailForm = () => {
         isAuthenticated && (
             <form ref={form} onSubmit={sendEmail}>
                 <label>Name</label>
-                <input type="text" name="user_name" />
+                <input type="text" name="from_name" />
                 <br/>
                 <label>From</label>
                 <input type="email" name="user_email" />
